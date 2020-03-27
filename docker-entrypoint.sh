d26ed0eef7c90c33570e139a269aee717d186e47
Romans Malinovskis <me@nearly.guru>
2020-03-27 15:22:44 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -290,7 +290,7 @@ fi
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-wp --allow-root theme install cyclingclub --activate
+#wp --allow-root theme install cyclingclub --activate
 
 
 exec "$@"
